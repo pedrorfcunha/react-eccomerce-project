053c1678899f9d222286bae242e0f98251fd768c
Pedro Cunha <pedrorfcunha@outlook.com>
2023-03-20 14:48:13 -0400

    dinamic routing included

--- src/components/product-item/product-item.component.jsx
+++ src/components/product-item/product-item.component.jsx
@@ -1,21 +1,25 @@
+import { Link } from "react-router-dom";
+
 import "./product-item.styles.scss";
 
 const ProductItem = ({ product }) => {
   const { gallery, name, id } = product;
 
   return (
-    <div className="product-container">
-      <div
-        className="background-image"
-        style={{ backgroundImage: `url(${gallery})` }}
-      ></div>
-      <div className="product-body-container">
-        <h2>
-          {name} and {id}
-        </h2>
-        <p>$50.00</p>
+    <Link to={`/product/${id}`}>
+      <div className="product-container">
+        <div
+          className="background-image"
+          style={{ backgroundImage: `url(${gallery})` }}
+        ></div>
+        <div className="product-body-container">
+          <h2>
+            {name} and {id}
+          </h2>
+          <p>$50.00</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
---
+++ src/routes/product-page/product-page.component.jsx
@@ -0,0 +1,11 @@
+import "./product-page.styles.scss";
+
+const ProductPage = () => {
+  return (
+    <div>
+      <h1>Product page</h1>
+    </div>
+  );
+};
+
+export default ProductPage;
