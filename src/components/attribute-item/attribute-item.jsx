162fdba73755ecb98b9895020136a646a50574cc
Pedro Cunha <pedrorfcunha@outlook.com>
2023-03-23 15:11:08 -0400

    improved code structure addressing feedbacks from pull request review

--- src/components/attribute-item/attribute-item.jsx
+++ src/components/attribute-item/attribute-item.jsx
@@ -9,41 +9,31 @@ const AttributeItem = ({ attribute }) => {
 
   const toggleSelectedBox = (item) => {
     setSelectedBox(item);
-    console.log(item);
-    console.log(selectedBox);
-  };
-
-  const getItemClass = (name) => {
-    if (name === "Color") {
-      return "attribute-color-container";
-    } else {
-      return "attribute-selector-container";
-    }
   };
 
   return (
     <div className="attribute-item">
       <h4 className="attribute-title">{name}</h4>
-      <div className={getItemClass(name)}>
-        {items.map((item) => {
-          const itemStyle =
-            getItemClass(name) === "attribute-color-container"
-              ? { backgroundColor: item.value }
-              : {};
-          return (
-            <span
-              key={item.id}
-              className={
-                "attribute-selector-box" +
-                (selectedBox === item.id ? " selected-box" : "")
-              }
-              style={itemStyle}
-              onClick={() => toggleSelectedBox(item.id)}
-            >
-              {item.value}
-            </span>
-          );
-        })}
+      <div
+        className={
+          name === "Color"
+            ? "attribute-color-container"
+            : "attribute-noncolor-container"
+        }
+      >
+        {items.map((item) => (
+          <span
+            key={item.id}
+            className={
+              "attribute-selector-box" +
+              (selectedBox === item.id ? " selected-box" : "")
+            }
+            style={name === "Color" ? { backgroundColor: item.value } : {}}
+            onClick={() => toggleSelectedBox(item.id)}
+          >
+            {item.value}
+          </span>
+        ))}
       </div>
     </div>
   );
