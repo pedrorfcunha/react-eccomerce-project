24098412e9fcc878f6d2516574ec30d3610b02bb
Pedro Cunha <pedrorfcunha@outlook.com>
2023-03-28 15:33:42 -0400

    included nav-link component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -9,6 +9,8 @@ import { CartContext } from "../../contexts/cart.context";
 
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
+import NavLink from "../../components/nav-link/nav-link.component";
+
 import { ReactComponent as ShopLogo } from "./../../assets/shoplogo.svg";
 import { ReactComponent as ArrowLogo } from "./../../assets/arrowlogo.svg";
 import { ReactComponent as CartLogo } from "./../../assets/cartlogo.svg";
@@ -16,34 +18,32 @@ import { ReactComponent as CurrencyLogo } from "./../../assets/currencylogo.svg"
 
 import "./navigation.styles.scss";
 
-const Navigation = () => {  
+const Navigation = () => {
   const [categories, setCategories] = useState([]);
-  
+
   const response = useQuery(GET_PRODUCTS);
   const { loading, error, data } = response;
-  
+
   useEffect(() => {
     if (data) {
       const { categories } = data;
       setCategories(categories);
     }
   }, [data]);
-  
+
   const { pathname } = useLocation();
   const currentPage = pathname.slice(1);
-  
+
   const [selectedPage, setSelectedPage] = useState(currentPage);
-  
-  const toggleSelectedPage = (category) => {
-    setSelectedPage(category);
-  };
 
   useEffect(() => {
     if (data && selectedPage === "") {
       const { categories } = data;
       setSelectedPage(categories[0].name);
+    } else if (selectedPage !== "") {
+      setSelectedPage(currentPage);
     }
-  }, [data, selectedPage]);
+  }, [data, currentPage]);
 
   const { currentUser } = useContext(UserContext);
   const { isCartOpen, setIsCartOpen } = useContext(CartContext);
@@ -55,22 +55,15 @@ const Navigation = () => {
       <header className="navigation">
         <div className="nav-links-container">
           {categories.map((category) => (
-            <div
-              className={
-                category.name === selectedPage
-                  ? "nav-link-box-active"
-                  : "nav-link-box"
-              }
+            <NavLink
+              path={category.name}
+              description={category.name}
+              selectedPage={selectedPage}
               key={category.name}
-              onClick={() => toggleSelectedPage(category.name)}
-            >
-              <Link className="nav-link-name" to={"/" + category.name}>
-                {category.name}
-              </Link>
-            </div>
+            />
           ))}
         </div>
-        <div onClick={() => toggleSelectedPage("")}>
+        <div onClick={() => setSelectedPage("")}>
           <Link className="logo-container" to="/">
             <ShopLogo />
           </Link>
@@ -78,25 +71,18 @@ const Navigation = () => {
         <div className="actions-container">
           <div className="action-container">
             {currentUser ? (
-              <div className="nav-link-box">
-                <span onClick={signOutUser} className="nav-link-name">
-                  {" "}
-                  SIGN OUT
-                </span>
-              </div>
+              <NavLink
+                path={"auth"}
+                description={"SIGN OUT"}
+                selectedPage={selectedPage}
+                click={signOutUser}
+              />
             ) : (
-              <div
-                className={
-                  selectedPage === "auth"
-                    ? "nav-link-box-active"
-                    : "nav-link-box"
-                }
-                onClick={() => toggleSelectedPage("auth")}
-              >
-                <Link className="nav-link-name" to="auth">
-                  SIGN IN
-                </Link>
-              </div>
+              <NavLink
+                path={"auth"}
+                description={"SIGN IN"}
+                selectedPage={selectedPage}
+              />              
             )}
           </div>
           <div className="action-container">
