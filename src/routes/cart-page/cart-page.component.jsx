672130a158ec9da6ef8446138f71e318c5d3b74f
Pedro Cunha <pedrorfcunha@outlook.com>
2023-05-08 16:22:42 -0400

    cart-summary component created

--- src/routes/cart-page/cart-page.component.jsx
+++ src/routes/cart-page/cart-page.component.jsx
@@ -1,29 +1,17 @@
 import { useContext } from "react";
 
 import { CartContext } from "../../contexts/cart.context";
-import { CurrencySwitcherContext } from "../../contexts/currency-switcher.context";
 
 import CartModal from "../../components/cart-modal/cart-modal.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import Button from "../../components/button/button.component";
 import CartContainer from "../../components/cart-container/cart-container.component";
-import PaymentForm from "../../components/payment-form/payment-form.component";
+import CartSummary from "../../components/cart-summary/cart-summary.component";
 
 import "./cart-page.styles.scss";
 
 const CartPage = () => {
-  const { isCartOpen, cartCount, cartTotalPrice } = useContext(CartContext);
-  const { currencySymbol } = useContext(CurrencySwitcherContext);
-
-  const formattedCartTotalPrice = cartTotalPrice.toLocaleString("en-US", {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  });
-
-  const cartTotalTax = (cartTotalPrice * 0.21).toLocaleString("en-US", {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  });
+  const { isCartOpen } = useContext(CartContext);
 
   return (
     <div>
@@ -35,28 +23,10 @@ const CartPage = () => {
       <div className="cart-page">
         <h1 className="cart-title">CART</h1>
         <CartContainer />
-        <div className="cart-summary">
-          <ol className="ol-title">
-            <li className="li-title">Tax 21%:</li>
-            <li className="li-title">Quantity:</li>
-            <li className="li-title total">Total:</li>
-          </ol>
-          <ol className="ol-data">
-            <li className="li-data">
-              {currencySymbol}
-              {cartTotalTax}
-            </li>
-            <li className="li-data">{cartCount}</li>
-            <li className="li-data">
-              {currencySymbol}
-              {formattedCartTotalPrice}
-            </li>
-          </ol>
-        </div>
+        <CartSummary />
         <div className="btn-container">
           <Button>ORDER</Button>
         </div>
-        <PaymentForm></PaymentForm>
       </div>
     </div>
   );
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -4,6 +4,8 @@ import { CartContext } from "../../contexts/cart.context";
 
 import CartModal from "../../components/cart-modal/cart-modal.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+import PaymentForm from "../../components/payment-form/payment-form.component";
+import CartSummary from "../../components/cart-summary/cart-summary.component";
 
 import "./checkout.styles.scss";
 
@@ -17,7 +19,14 @@ const Checkout = () => {
           <CartDropdown />
         </CartModal>
       )}
-      <div>TESTE</div>
+      <div className="checkout-container">
+        <div className="payments-container">
+          <PaymentForm />
+        </div>
+        <div className="cart-summary-container">
+          <CartSummary />
+        </div>
+      </div>
     </div>
   );
 };
