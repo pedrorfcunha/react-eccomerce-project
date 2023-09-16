c9c87ae5ef92770a3a0a85b5a76b392a42f00ceb
Pedro Cunha <pedrorfcunha@outlook.com>
2023-09-16 16:25:07 -0400

    fix stripe key

--- src/utils/stripe/stripe.utils.js
+++ src/utils/stripe/stripe.utils.js
@@ -1,5 +1,5 @@
-import { loadStripe } from "@stripe/stripe-js";
+import { loadStripe } from '@stripe/stripe-js';
 
 export const stripePromise = loadStripe(
-  process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
+  'pk_test_51MYwlpFA1rEClbWwvFF1KzRawOJyt5IARiJzzXjq8gdRS5t2RASqQKMfsrHSI0ysyKYFbHxhmWOqwYnCz01iQMpZ00Uv8J5KEY',
 );
